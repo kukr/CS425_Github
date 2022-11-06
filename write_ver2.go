197d50691a92e651a4bb0343a65000b5b95b71b5
219sansim <108684604+219sansim@users.noreply.github.com>
2022-11-06 00:01:15 -0500

    Add files via upload

--- write_ver2.go
+++ write_ver2.go
@@ -3,53 +3,41 @@ import ("os/exec"
 		"fmt"
 		"hash/crc32"
 		"strconv")
+membership_list := [] //Need a genuine memebershiplist
+num_replicas := 0
 func hashcode(s string) int {
 	v := int(crc32.ChecksumIEEE([]byte(s)))
 	return v%len(membership_list)
 }
-func inform_nodes(sdfsFileName string, nodeid int){
-	for(i=0;i<len(membership_list);i++){
-		conn, err := net.Dial("udp", getHostFromId(i))
+func inform_nodes_insert_into_sdfds(sdfsFileName string, nodeid int){
+	for i=0;i<len(membership_list);i++ {
+		conn, err := net.Dial("tcp", get_hostname_from_id(nodeid)+":8080")
 		if err != nil {
-			fmt.Printf("Some error %v", err)
+			log.Fatal("Connection error", err)
 		}
-		fmt.Fprintf(conn, "written "+ sdfsFileName + "to" + getHostFromId(nodeid))
+		encoder := gob.NewEncoder(conn)
+		p := &message{0,sdfsFileName,nodeid}
+		encoder.Encode(p)
+		conn.Close()
 	}
 }
 func putfile(sdsFilename string, localfilename string){
-	if 
 	hash_val := hashcode(filename)
 	version_no, ok := *fileTabVerMan[sdfsFileName];
     if !ok {
         version_no = 0;
 	   }
 	version_no += 1
-	cmd := exec.Command("scp", localfilename , getHostFromId(hash_val%(len(membership_list))) + "primary/"+ sdfsFilename + strconv.Itoa(version_no))//get_hostname_from_id
+	for (i=0; i<num_replicas;i++){ //set num_replicas
+	cmd := exec.Command("scp", localfilename , getHostFromId((hash_val+i)%(len(membership_list))) + sdfsFilename + strconv.Itoa(version_no))//get_hostname_from_id
 	err := cmd.Run()
 	if err != nil{
-	fmt.Println("copy error", err)
+	fmt.Println("putfile copy error" + getHostFromId((hash_val+i)%(len(membership_list))), err)
 }
 else{
-insertfile(sdfsFileName,[hash_val])
-inform_nodes(sdfsFileName, hash_val)
+insertfile(sdfsFileName,[hash_val+i])
+inform_nodes_insert_into_sdfds(sdfsFileName, hash_val+i)
 }
-cmd = exec.Command("scp", localfilename , getHostFromId((hash_val+1)%(len(membership_list))) +"replica_1/" + sdsFilename + strconv.Itoa(version_no))//get_hostname_from_id
-err = cmd.Run()
-if err != nil{
-fmt.Println("copy error", err)
-}
-else{
-	insertfile(sdfsFileName,[hash_val+1])
-	inform_nodes(sdfsFileName, hash_val+1)
-}
-cmd = exec.Command("scp", localfilename , getHostFromId((hash_val+2)%(len(membership_list))) +"replica_2/" + sdfsFilename + strconv.Itoa(version_no))//get_hostname_from_id
-err = cmd.Run()
-if err != nil{
-fmt.Println("copy error", err)
-}
-else{
-insertfile(sdfsFileName,[hash_val+2])
-inform_nodes(sdfsFileName, hash_val+2)
 }
 }
 func main(){
